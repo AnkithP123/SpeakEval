461f12f76c7e74c064a20fc4917c0e511d493348
AnkithP123 <143125301+AnkithP123@users.noreply.github.com>
2024-09-04 20:06:43 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Room from './pages/Room'
 import CreateRoom from './pages/CreateRoom'
 import TeacherPortalRoom from './pages/TeacherPortalRoom'
 import TeacherPortalRouter from './pages/TeacherPortalRouter'
-import AudioPlayer from './pages/AudioPlayer'
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 
 
@@ -22,7 +21,6 @@ function App() {
         <Route path="/room/:roomCode" element={<Room />} />
         <Route path="/teacher-portal" element={<TeacherPortalRouter />} />
         <Route path="/teacher-portal/:roomCode" element={<TeacherPortalRoom />} />
-        <Route path="/audio" element={<AudioPlayer />} />
         <Route path="*" element={<Maintainence />} />
       </Route>
       // <Route path="/" element={<MainLayout />}>
