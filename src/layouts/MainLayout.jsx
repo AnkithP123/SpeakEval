ebf6524b91fd6739d9ef57c636bffcd96b178e03
AnkithP123 <143125301+AnkithP123@users.noreply.github.com>
2024-09-12 10:47:25 -0700

    Add files via upload

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { ToastContainer } from 'react-toastify';
+import BottomBar from '../components/BottomBar';
 import 'react-toastify/dist/ReactToastify.css';
 
 function MainLayout() {
   return (
-    <div className='bg-blue-200 min-h-screen'>
+    <div className='bg-blue-200 min-h-screen flex flex-col'>
       <Navbar />
-      <Outlet />
+      <div className='flex-grow'>
+        <Outlet />
+      </div>
       <ToastContainer />
+      <BottomBar className='mt-auto' />
     </div>
   );
 }
