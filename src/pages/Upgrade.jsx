b79032782a8bab833b99b6aaa7d9b29f4df59a96
Dev-Zenos <158857144+Dev-Zenos@users.noreply.github.com>
2024-10-22 21:42:43 -0700

    Update Upgrade.jsx

--- src/pages/Upgrade.jsx
+++ src/pages/Upgrade.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import UpgradePanel from '../components/UpgradePanel'
+import React from 'react';
+import UpgradePanel from '../components/UpgradePanel';
 
 function Upgrade() {
-    return (
-        <div className="flex justify-center items-center h-screen" style={{ transform: 'translateY(-75px)' }}>
-            <UpgradePanel />
-            <UpgradePanel basicCard={false} />
-        </div>
-    )
+  return (
+    <div className="flex justify-center items-center h-screen z-50 fixed inset-0" style={{ transform: 'translateY(-75px)' }}>
+      <div className="bg-blue-100 bg-opacity-[80%] p-8 rounded-[25px] shadow-lg flex space-x-6">
+        <UpgradePanel />
+        <UpgradePanel basicCard={false} />
+      </div>
+    </div>
+  );
 }
 
-export default Upgrade
+export default Upgrade;
