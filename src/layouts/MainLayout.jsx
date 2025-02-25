17ae5221f0caf9d1612fb6213a6f99824ca75779
AnkithP123 <143125301+AnkithP123@users.noreply.github.com>
2025-02-25 08:54:36 -0800

    maintenance

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,9 +3,21 @@ import Navbar from '../components/Navbar';
 import { ToastContainer } from 'react-toastify';
 import { Analytics } from "@vercel/analytics/react"
 import BottomBar from '../components/BottomBar';
+// import cuteAlert from 'cute-alert';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function MainLayout({set, set2, set3, set4}) {
+  useEffect(() => {
+    toast.error("SpeakEval is undergoing slight maintenance. Some features may not work as expected, such as executing exams. We apologize for the inconvenience. Please check back later.", {
+      position: "top-center",
+      autoClose: false,
+      closeOnClick: true,
+      draggable: true,
+      progress: undefined,
+    });
+  });
   return (
     <div className='bg-blue-200 min-h-screen flex flex-col'>
       <Analytics />
