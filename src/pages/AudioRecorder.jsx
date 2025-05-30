fca29150e484245385832cea192dc35e55487941
Dev-Zenos <158857144+Dev-Zenos@users.noreply.github.com>
2025-05-30 11:57:21 -0700

    does it work?

--- src/pages/AudioRecorder.jsx
+++ src/pages/AudioRecorder.jsx
@@ -663,6 +663,7 @@ export default function AudioRecorder({ code, participant, uuid }) {
   };
 
   const stopRecording = () => {
+    if (!isRecording) return;
     console.log("Stopping recording...");
     setStopped(true);
     setIsRecording(false);
