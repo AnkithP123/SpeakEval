32f16004c1b0da0481e8dd631c19bd3b2b3d803c
AnkithP123 <143125301+AnkithP123@users.noreply.github.com>
2025-08-03 21:10:26 -0700

    readd warning

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -23,14 +23,14 @@ document.documentElement.style.setProperty(
   document.documentElement.style.getPropertyValue("--cute-alert-min-width") ||
     "40%"
 );
-/*cuteAlert({
+cuteAlert({
   type: "warning",
   id: "cute-alert-indev",
   title: "Platform Update In Progress",
   description:
     "We're currently rewriting parts of the platform to better support teachers.\nYou may experience some temporary issues over the next few days.\nIf anything isn't working as expected, please contact us at info@speakeval.org.",
   primaryButtonText: "OK",
-});*/
+});
 
 const isMobile = isMobileDevice();
 if (isMobile) {
