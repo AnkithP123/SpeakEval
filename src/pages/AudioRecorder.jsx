856c6b0f08a0ef8963bfeac3221b0cb80f6ba1b8
Dev-Zenos <158857144+Dev-Zenos@users.noreply.github.com>
2025-08-10 22:27:33 -0700

    Update AudioRecorder.jsx

--- src/pages/AudioRecorder.jsx
+++ src/pages/AudioRecorder.jsx
@@ -1144,6 +1144,7 @@ export default function AudioRecorder() {
     };
 
     const onFullscreenChange = () => {
+      return;
       console.log("🖥️ Fullscreen change detected:", {
         fullscreenElement: !!document.fullscreenElement,
         isRecording,
